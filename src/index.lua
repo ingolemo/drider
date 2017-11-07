09813f8fd27202ea2bdd0c88f88c45dc3d567f8e
Adrian Room <ingolemo@gmail.com>
2017-11-07 08:14:04 +0000

    make import code handle sd-card loading

--- src/index.lua
+++ src/index.lua
@@ -5,7 +5,12 @@ function import(module_name)
 	if mod ~= nil then
 		return mod
 	end
-	local module_path = 'romfs:/' .. module_name .. '.lua'
+
+	local module_path = '/3ds/drider/' .. module_name .. '.lua'
+	if not System.doesFileExist(module_path) then
+		module_path = 'romfs:/' .. module_name .. '.lua'
+	end
+
 	mod = dofile(module_path)
 	imported[module_name] = mod
 	return mod
